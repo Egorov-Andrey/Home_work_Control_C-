32e9dac33acc3eefa8e6a73b8887feaa0214134f
Egorov-Andrey <andreye1989@gmail.com>
2023-02-13 13:54:44 +0300

    creation array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int[] array = {674, 1, 897, 25};
+
+int n = array.Length;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] < 100)
+    {
+        Console.WriteLine(array[index]);
+    }
+    index++;
+}
+
+
+
